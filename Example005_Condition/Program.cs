64d66ac7c139f15fed770c9a2410b4771b47337d
Julia Swan <julie112397@gmail.com>
2022-10-14 19:20:29 +0500

    Example 005 Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter your name: ");
+string UserName = Console.ReadLine();
+if(UserName.ToLower() == "julia") Console.WriteLine("Heey, it's my dear JULIA!");
+else 
+{   
+    Console.Write("Hello, ");
+    Console.WriteLine(UserName);
+}
