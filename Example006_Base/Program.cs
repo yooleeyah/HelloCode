990e09ab6ff3b6a4f76d5578f2afefcecd43a1c4
Julia Swan <julie112397@gmail.com>
2022-10-14 19:28:46 +0500

    Example 006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 23;
+int b = 11;
+int c = 33;
+int d = 6;
+int e = 4;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
